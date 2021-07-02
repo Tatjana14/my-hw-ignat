c323da2f9a24a0e7cfe259892ab0ffa3be55b7ae
Tatjana14 <tanycha1403@gmail.com>
2021-07-02 17:55:03 +0300

    all hw1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -12,10 +12,12 @@ function Message(props: MessageType) {
     return (
         <div className={s.container}>
             <img className={s.avatar} src={props.avatar}/>
-            <div className={s.message}>
-                <p>{props.name}</p>
-                <span>{props.message}</span>
-                <div className={s.time} >
+            <div className={s.message_wrapper}>
+                <div className={s.message_text}>
+                    <p className={s.message_author}>{props.name}</p>
+                    <span className={s.message_text}>{props.message}</span>
+                </div>
+                <div className={s.message_time} >
                     <span>{props.time}</span>
                 </div>
             </div>
